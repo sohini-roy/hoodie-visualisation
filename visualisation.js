40afa835a34552a6172ae319971f6193c2cccae2
sohini-roy <sohinirc45@gmail.com>
2017-04-12 14:51:07 +0530

    indenation fixed

--- visualisation.js
+++ visualisation.js
@@ -14,12 +14,8 @@ var create_repos = function(arguments,repolist) {
   for(var i=0;i <repos.length;i++){
     var dummyLabelDictionary = {};
     var repo_issues = repos[i].issues
-    // console.log(repos[i].issues);
-    // console.log(repo_issues);
     for(var j= 0;j<repo_issues.length;j++){
       var repo_issues_label = repo_issues[j].labels
-      // console.log(repos[i].issues[j].labels);
-      // console.log(repo_issues_label);
       if(repo_issues_label.length==0){
         if(!dummyLabelDictionary['no labels']){
           dummyLabelDictionary['no labels'] = [];
@@ -28,8 +24,6 @@ var create_repos = function(arguments,repolist) {
       }
       for(var k=0;k<repo_issues_label.length;k++){
         var repo_issues_labeldummy = repos[i].issues[j].labels[k]
-        // console.log(repos[i].issues[j].labels[k].name)
-        // console.log(repo_issues_labeldummy);
         if(!dummyLabelDictionary[repo_issues_labeldummy.name]){
             dummyLabelDictionary[repo_issues_labeldummy.name] = [];
         }
@@ -37,13 +31,11 @@ var create_repos = function(arguments,repolist) {
       }
     }
     repos[i]['labelDictionary'] = dummyLabelDictionary;
-    // console.log(dummyLabelDictionary);
   }
   return repos;
 }
 
 function extract_issue_link(issue_list){
-  // console.log(labelDictionary);
   let issue_name_link = [];
   for(i = 0; i < issue_list.length; i++) {
     issue_name_link.push(issue_list[i].html_url);
@@ -52,7 +44,6 @@ function extract_issue_link(issue_list){
 }
 
 function extract_issue_name(issue_list){
-  // console.log(labelDictionary);
   let issue_name_list = [];
   for(i = 0; i < issue_list.length; i++) {
     issue_name_list.push(issue_list[i].title);
@@ -66,7 +57,6 @@ function create_node(repos){
   var clusters = [];
   for(var i = 0;i<repos.length;i++){
     for(key in (repos[i].labelDictionary)){
-      // console.log(repos[i]);
       var obj = {cluster : i,
                  repo_name : repos[i].name,
                  label_name : key,
@@ -87,7 +77,6 @@ function create_node(repos){
       }
     }
   }
-  console.log(nodedummy);
   return {nodes : nodedummy, clusters : clusters};
 }
 
@@ -129,154 +118,150 @@ function getrepo(){
   }
 
   $.when.apply($,repoDummyPromises).then(function(){
-
     var repos = create_repos(arguments,repolist); // total number of nodes
     var m = repos.length; // number of distinct clusters
     var obj = create_node(repos);
     var nodes = obj.nodes;
     var clusters = obj.clusters;
     var n = nodes.length
-    // console.log(nodes);
+    var color = d3.scale.category10()
+    .domain(d3.range(m));
 
-        var color = d3.scale.category10()
-            .domain(d3.range(m));
+    // The largest node for each cluster.
 
-        // The largest node for each cluster.
+    // var nodes = d3.range(n).map(function() {
+    //   var i = node.length,
+    //       r = node.issues_in_label * maxRadius,
+    //       d = {
+    //         cluster: i,
+    //         radius: r, 40 Inch
+    //         x: Math.cos(i / m * 2 * Math.PI) * 200 + width / 2 + Math.random(),
+    //         y: Math.sin(i / m * 2 * Math.PI) * 200 + height / 2 + Math.random()
+    //       };
+    //   if (!clusters[i] || (r > clusters[i].radius)) clusters[i] = d;
+    //   return d;
+    // });
 
-        // var nodes = d3.range(n).map(function() {
-        //   var i = node.length,
-        //       r = node.issues_in_label * maxRadius,
-        //       d = {
-        //         cluster: i,
-        //         radius: r, 40 Inch
-        //         x: Math.cos(i / m * 2 * Math.PI) * 200 + width / 2 + Math.random(),
-        //         y: Math.sin(i / m * 2 * Math.PI) * 200 + height / 2 + Math.random()
-        //       };
-        //   if (!clusters[i] || (r > clusters[i].radius)) clusters[i] = d;
-        //   return d;
-        // });
+    var force = d3.layout.force()
+    .nodes(nodes)
+    .size([width, height])
+    .gravity(0.02)
+    .charge(0)
+    .on("tick", tick)
+    .start();
 
-        var force = d3.layout.force()
-            .nodes(nodes)
-            .size([width, height])
-            .gravity(0.02)
-            .charge(0)
-            .on("tick", tick)
-            .start();
+    var tip = d3.tip()
+    .attr('class', 'd3-tip')
+    .offset([-10, 0])
+    .html(function(d) {
+      return "<span style = 'font-size:22px'><b>Label Name: </b><span style='color:red'>" + d.label_name + "</span><br><b>Number of Issues: </b><span style='color:red'>" + d.total_issues + "</span></span>";
+    })
 
-        var tip = d3.tip()
-            .attr('class', 'd3-tip')
-            .offset([-10, 0])
-            .html(function(d) {
-            return "<span style = 'font-size:22px'><b>Label Name: </b><span style='color:red'>" + d.label_name + "</span><br><b>Number of Issues: </b><span style='color:red'>" + d.total_issues + "</span></span>";
-            })
+    // var tip_issue_list = d3.tip()
+    //                        .attr('class', 'd3.tip')
+    //                        .offset([0,0])
+    //                        .(function(d) {
+    //                          return(d.issues_list)
+    //                        })
 
-        // var tip_issue_list = d3.tip()
-        //                        .attr('class', 'd3.tip')
-        //                        .offset([0,0])
-        //                        .(function(d) {
-        //                          return(d.issues_list)
-        //                        })
+    var svg = d3.select("#node").append("svg:svg")
+    .attr("width", width)
+    .attr("height", height);
 
-        var svg = d3.select("#node").append("svg:svg")
-            .attr("width", width)
-            .attr("height", height);
+    svg.call(tip);
 
-        svg.call(tip);
+    // var svg = d3.select("#issue_list").append("svg:svg")
+    //             .attr("width", width)
+    //             .attr("height", height);
 
-        // var svg = d3.select("#issue_list").append("svg:svg")
-        //             .attr("width", width)
-        //             .attr("height", height);
+    // svg.call(tip_issue_list);
 
-        // svg.call(tip_issue_list);
+    function populate(issues_list, issues_link) {
+      $('#issue-list').empty();
+      for(var i=0;i<issues_list.length;i++){
+        $('#issue-list').append('<li style="padding:2px"><a style="color:blue;text-decoration: none;" href='+issues_link[i]+'>'+issues_list[i]+'</a></li>');
+      }
+    }
 
-        function populate(issues_list, issues_link) {
-          $('#issue-list').empty();
-          for(var i=0;i<issues_list.length;i++){
-            $('#issue-list').append('<li style="padding:2px"><a style="color:blue;text-decoration: none;" href='+issues_link[i]+'>'+issues_list[i]+'</a></li>');
-          }
+    var node = svg.selectAll("circle")
+    .data(nodes)
+    .enter().append("circle")
+    .style("fill", function(d) { return color(d.cluster); })
+    .call(force.drag)
+    .on('mouseover', tip.show)
+    .on('mouseout', tip.hide)
+    .on('click', function(d) {
+      populate(d.issues_list, d.issues_link);
+    })
+
+    // var titles = node.append('svg:title')
+    //                   .text(function(d){
+    //                     return d.label_name+" ("+d.total_issues+")";
+    //                   });
+
+    node.transition()
+    .duration(750)
+    .delay(function(d, i) { return i * 5; })
+    .attrTween("r", function(d) {
+      var i = d3.interpolate(0, d.radius);
+      return function(t) { return d.radius = i(t); };
+    });
+
+    function tick(e) {
+      node
+      .each(cluster(10 * e.alpha * e.alpha))
+      .each(collide(0.5))
+      .attr("cx", function(d) { return d.x; })
+      .attr("cy", function(d) { return d.y; });
+    }
+
+    // Move d to be adjacent to the cluster node.
+    function cluster(alpha) {
+      return function(d) {
+        var cluster = clusters[d.cluster];
+        if (cluster === d) return;
+        var x = d.x - cluster.x,
+        y = d.y - cluster.y,
+        l = Math.sqrt(x * x + y * y),
+        r = d.radius + cluster.radius;
+        if (l != r) {
+          l = (l - r) / l * alpha;
+          d.x -= x *= l;
+          d.y -= y *= l;
+          cluster.x += x;
+          cluster.y += y;
         }
+      };
+    }
 
-        var node = svg.selectAll("circle")
-            .data(nodes)
-            .enter().append("circle")
-            .style("fill", function(d) { return color(d.cluster); })
-            .call(force.drag)
-            .on('mouseover', tip.show)
-            .on('mouseout', tip.hide)
-            .on('click', function(d) {
-             populate(d.issues_list, d.issues_link);
-            })
-            // .on('mouseout', tip_issue_list.hide);
-
-        // var titles = node.append('svg:title')
-        //                   .text(function(d){
-        //                     return d.label_name+" ("+d.total_issues+")";
-        //                   });
-
-        node.transition()
-            .duration(750)
-            .delay(function(d, i) { return i * 5; })
-            .attrTween("r", function(d) {
-              var i = d3.interpolate(0, d.radius);
-              return function(t) { return d.radius = i(t); };
-            });
-
-        function tick(e) {
-          node
-              .each(cluster(10 * e.alpha * e.alpha))
-              .each(collide(0.5))
-              .attr("cx", function(d) { return d.x; })
-              .attr("cy", function(d) { return d.y; });
-        }
-
-        // Move d to be adjacent to the cluster node.
-        function cluster(alpha) {
-          return function(d) {
-            var cluster = clusters[d.cluster];
-            if (cluster === d) return;
-            var x = d.x - cluster.x,
-                y = d.y - cluster.y,
-                l = Math.sqrt(x * x + y * y),
-                r = d.radius + cluster.radius;
-            if (l != r) {
+    // Resolves collisions between d and all other circles.
+    function collide(alpha) {
+      var quadtree = d3.geom.quadtree(nodes);
+      return function(d) {
+        var r = d.radius + maxRadius + Math.max(padding, clusterPadding),
+        nx1 = d.x - r,
+        nx2 = d.x + r,
+        ny1 = d.y - r,
+        ny2 = d.y + r;
+        quadtree.visit(function(quad, x1, y1, x2, y2) {
+          if (quad.point && (quad.point !== d)) {
+            var x = d.x - quad.point.x,
+            y = d.y - quad.point.y,
+            l = Math.sqrt(x * x + y * y),
+            r = d.radius + quad.point.radius + (d.cluster === quad.point.cluster ? padding : clusterPadding);
+            if (l < r) {
               l = (l - r) / l * alpha;
               d.x -= x *= l;
               d.y -= y *= l;
-              cluster.x += x;
-              cluster.y += y;
+              quad.point.x += x;
+              quad.point.y += y;
             }
-          };
-        }
-
-        // Resolves collisions between d and all other circles.
-        function collide(alpha) {
-          var quadtree = d3.geom.quadtree(nodes);
-          return function(d) {
-            var r = d.radius + maxRadius + Math.max(padding, clusterPadding),
-                nx1 = d.x - r,
-                nx2 = d.x + r,
-                ny1 = d.y - r,
-                ny2 = d.y + r;
-            quadtree.visit(function(quad, x1, y1, x2, y2) {
-              if (quad.point && (quad.point !== d)) {
-                var x = d.x - quad.point.x,
-                    y = d.y - quad.point.y,
-                    l = Math.sqrt(x * x + y * y),
-                    r = d.radius + quad.point.radius + (d.cluster === quad.point.cluster ? padding : clusterPadding);
-                if (l < r) {
-                  l = (l - r) / l * alpha;
-                  d.x -= x *= l;
-                  d.y -= y *= l;
-                  quad.point.x += x;
-                  quad.point.y += y;
-                }
-              }
-              return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
-            });
-          };
-        }
-      })
+          }
+          return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+        });
+      };
     }
+  })
+}
 
 getrepo();
